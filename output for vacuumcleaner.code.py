c0296c9c294e53d9d8ce14c3fdb6e0ea04d9e056
hanmanthrao <hanmanthraobd@gmail.com>
2025-09-01 12:40:30 +0530

    Create output for vacuumcleaner.code.py

---
+++ output for vacuumcleaner.code.py
@@ -0,0 +1,21 @@
+Choose a Vacuum Cleaner Shape:
+1. Circle
+2. Square
+3. Triangle
+4. Hexagon
+Enter your choice (1-4): 1
+
+You chose Circle Vacuum Cleaner.
+
+Enter command (start/stop/left/right/dock/exit): start
+Circle vacuum started cleaning.
+Enter command (start/stop/left/right/dock/exit): stop
+Circle vacuum stopped cleaning.
+Enter command (start/stop/left/right/dock/exit): left
+Circle vacuum must be started first.
+Enter command (start/stop/left/right/dock/exit): right
+Circle vacuum must be started first.
+Enter command (start/stop/left/right/dock/exit): dock
+Circle vacuum returned to dock.
+Enter command (start/stop/left/right/dock/exit): exit
+Exiting program. Goodbye!

a8fb435c09bef93eb703e56f64dfd8bff2d8066a
hanmanthrao <hanmanthraobd@gmail.com>
2025-09-01 23:03:15 +0530

    Create output for ratescape.code.py

---
+++ output for ratescape.code.py
@@ -0,0 +1,11 @@
+ DFS
+Path: [0, 1, 4, 5, 2, 3], Cost: 16, Visited: 5
+
+ BFS 
+Path: [0, 1, 2, 3], Cost: 9, Visited: 4
+
+ Dijkstra 
+Path: [0, 1, 2, 3], Cost: 9, Visited: 5
+
+ A* Search 
+Path: [0, 1, 2, 3], Cost: 9, Visited: 3

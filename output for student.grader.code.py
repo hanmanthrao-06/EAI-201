1f9c7acc1f2f210c629bbabe1690fa313ebd0232
hanmanthrao <hanmanthraobd@gmail.com>
2025-09-01 12:39:33 +0530

    Create output for student.grader.code.py

---
+++ output for student.grader.code.py
@@ -0,0 +1,18 @@
+Enter number of subjects: 4
+Enter name of subject 1: python
+Enter name of subject 2: japanese
+Enter name of subject 3: DSA
+Enter name of subject 4: AIML
+Enter marks obtained in python (out of 100): 89
+Enter marks obtained in japanese (out of 100): 70
+Enter marks obtained in DSA (out of 100): 75
+Enter marks obtained in AIML (out of 100): 88
+
+Student Report Card:
+python marks = 89 , grade = A
+japanese marks = 70 , grade = B
+DSA marks = 75 , grade = B
+AIML marks = 88 , grade = A
+
+Overall Average = 80.50
+Final Grade = A
